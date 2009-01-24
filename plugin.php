1f507673544dcacfe81d38766ff06ff04c61d3a1
satollo <satollo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-24 23:02:28 +0000

    git-svn-id: https://plugins.svn.wordpress.org/post-layout/trunk@88953 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Post Layout
 Plugin URI: http://www.satollo.com/english/wordpress/post-layout
 Description: Adds HTML o javascript code before, after or in the middle of the content of pages or posts without modify the theme. For any problem or question write me: satollo@gmail.com.
-Version: 2.0.1
+Version: 2.0.2
 Author: Satollo
 Author URI: http://www.satollo.com
 Disclaimer: Use at your own risk. No warranty expressed or implied is provided.
@@ -101,6 +101,10 @@ function pstl_the_content(&$content)
         {
             $length = strlen(strip_tags($content));
             if ($length >= $pstl_options['post_more_size']) $more = $pstl_options['post_more' . $suffix]; 
+        }
+        else 
+        {
+            $more = $pstl_options['post_more' . $suffix];
         }
     }
     else if (is_page())
